6c47e8fc0dfc4f1683220f81c2d0b2bbe9d7ef63
trollerpc <alifserg@gmail.com>
2018-11-18 20:31:30 +0200

    First commit

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-$x = "hello world";
-echo $x;
+// regular expressions
+// preg_match, preg_replace
+
+$string = "ученик сидит за партой";
+$pattern = "/ученик/";
+
+$result = preg_match ($pattern, $string);
+
+//preg_match ищет совпадения,принимает 2 параметра, возвращает true/false
+
+var_dump ($result);
+
+echo $string;
 ?>
+
