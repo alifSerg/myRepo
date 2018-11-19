c8b834a8873b14bcc5e8995ab0c737d2f040f738
trollerpc <alifserg@gmail.com>
2018-11-19 23:22:48 +0200

    .

--- components/Router.php
+++ components/Router.php
@@ -5,9 +5,20 @@ class Router {
 
     public function __construct ()
     {
+         $routesPath = ROOT. "/config/routes.php"; //путь к файлу с маршрутами и к коренной папке
+         $this->routes = include($routesPath); //присвоение свойству routes массив, который ханится в файле rotes.php при помощи метода include
 
     }
 
+//метод run: получить строку запроса
+
+//проверить наличие такого запроса в rotes.php
+
+//если есть совпадение, определить какой контроллер и action обрабатывают запрос
+
+//подключить файл класса контроллера
+
+//создать объект, вызвать метод (action)
     public function run () //принимает отправления от фронт контроллера
     {
          echo "Class Router, method run";
