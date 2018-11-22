7ff3e849f61a3d348b85243f6ce34684e939a7ee
trollerpc <alifserg@gmail.com>
2018-11-22 18:39:12 +0200

    added desctiption for newsController

--- controllers/newsController.php
+++ controllers/newsController.php
@@ -1,6 +1,12 @@
 <?php
 class NewsController {
     public function actionIndex() {
+        echo "Список новостей";
+        return true;
+    }
+
+    public function actionView() {
+        echo "просмотр одной новости"
         return true;
     }
 }
